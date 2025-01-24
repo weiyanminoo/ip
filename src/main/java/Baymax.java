fcce72771529379e96c5531f7ccb18d276edf6bb
weiyanminoo <>
2025-01-24 12:12:04 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Baymax.java
@@ -0,0 +1,14 @@
+public class Baymax {
+    public static void main(String[] args) {
+
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hey there! I'm Baymax"); // Replace "Helix" with your chatbot name
+        System.out.println(" How may I assist you?");
+        System.out.println("____________________________________________________________");
+
+        // Exit message
+        System.out.println("____________________________________________________________");
+        System.out.println(" Byeee! Have a great day ahead :)");
+        System.out.println("____________________________________________________________");
+    }
+}
