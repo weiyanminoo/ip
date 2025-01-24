65d565997e49cbad3eaa04fb56eba6e7841c2c4f
weiyanminoo <>
2025-01-24 12:30:21 +0800

    Echo

--- src/main/java/Baymax.java
+++ src/main/java/Baymax.java
@@ -1,14 +1,35 @@
+import java.util.Scanner;
+
 public class Baymax {
+
     public static void main(String[] args) {
-
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hey there! I'm Baymax"); // Replace "Helix" with your chatbot name
+        // Greeting message
+        System.out.println("==========================================");
+        System.out.println(" Hey there! I'm Baymax");
         System.out.println(" How may I assist you?");
-        System.out.println("____________________________________________________________");
+        System.out.println("==========================================");
 
-        // Exit message
-        System.out.println("____________________________________________________________");
-        System.out.println(" Byeee! Have a great day ahead :)");
-        System.out.println("____________________________________________________________");
+        // Get input from user
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        // echos user's input
+        while (true) {
+
+            input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("==========================================");
+                System.out.println(" Byeee! Hope to see you again soon!");
+                System.out.println("==========================================");
+                break;
+            }
+
+            System.out.println("==========================================");
+            System.out.println(" " + input);
+            System.out.println("==========================================");
+        }
+
+        scanner.close();
     }
 }
